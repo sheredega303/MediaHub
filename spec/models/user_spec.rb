ecedb2ea83a7346c12ac53de43668724e2fca2c3
Andrey Sheredega <sheredega303@gmail.com>
2024-10-14 12:53:10 +0300

    Create factories and update model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_length_of(:channel_name).is_at_most(100) }
 
   it { is_expected.to have_many(:videos).dependent(:destroy) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,4 +7,5 @@ RSpec.describe Video, type: :model do
   it { is_expected.to validate_length_of(:title).is_at_most(255) }
   it { is_expected.to validate_presence_of(:age_rating) }
   it { is_expected.to validate_inclusion_of(:age_rating).in_array(%w[G PG PG-13 R NC-17]) }
+  it { is_expected.to have_one_attached(:file) }
 end
